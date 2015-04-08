dfe2d97e422b9f5e285010ee2153438bad7348cc
NightFarmer <494090597@qq.com>
2015-04-09 02:14:21 +0800

    hehe

---
+++ ViewDragHelperTest/src/com/flavienlaurent/vdh/DragMainLayout.java
@@ -0,0 +1,56 @@
+package com.flavienlaurent.vdh;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+
+import com.flavienlaurent.vdh.YoutubeLayout3.DragState;
+
+public class DragMainLayout extends RelativeLayout{
+
+	private YoutubeLayout3 dragLayout;
+
+	public DragMainLayout(Context context) {
+		this(context, null);
+	}
+
+	public DragMainLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public DragMainLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	
+	public void setDragLayout(YoutubeLayout3 dragLayout){
+		this.dragLayout = dragLayout;
+	}
+	
+	/**
+	 * 如果点击到本viewgroup的子控件，是否把touch转交给子控件。返回true则拦截，拦截够会触发这个group的touch事件
+	 */
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		DragState state = dragLayout.getState();
+		if (!DragState.CLOSE.equals(state)) {
+			return true;
+		}
+		return super.onInterceptTouchEvent(ev);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+//		DragState state = dragLayout.getState();
+//		Log.i("qq", ""+state+"-"+event.getAction());
+//		if (!DragState.CLOSE.equals(state)) {
+//			if (event.getAction() == MotionEvent.ACTION_UP) {
+//				dragLayout.close();
+//			}
+//			return true;
+//		}
+		return super.onTouchEvent(event);
+	}
+	
+}
--- ViewDragHelperTest/src/com/flavienlaurent/vdh/YoutubeLayout3.java
+++ ViewDragHelperTest/src/com/flavienlaurent/vdh/YoutubeLayout3.java
@@ -1,19 +1,25 @@
 package com.flavienlaurent.vdh;
 
+import com.nineoldandroids.view.ViewHelper;
+
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 public class YoutubeLayout3 extends FrameLayout{
 
 	private ViewDragHelper mDragHelper;
-	private View header;
+	private DragMainLayout header;
 	private View desc;
 	
+	private int maxLeft;
 //	private int headerLeft;
 	
 	/**
@@ -24,6 +30,8 @@ public class YoutubeLayout3 extends FrameLayout{
 	 * 主界面移动比例，0.7为左边界能够移动到的最大屏幕比例
 	 */
 	private final float MAINLOCATPERSONT = 0.7f;
+	private int x_down;
+	private int y_down;
 
 	public YoutubeLayout3(Context context) {
 		this(context, null);
@@ -40,14 +48,32 @@ public class YoutubeLayout3 extends FrameLayout{
 	
 	@Override
 	protected void onFinishInflate() {
-		header = findViewById(R.id.header);
+		header = (DragMainLayout) findViewById(R.id.header);
 		desc = findViewById(R.id.desc);
+		header.setDragLayout(this);
+//		header.setClickable(true);
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		// TODO Auto-generated method stub
 		mDragHelper.processTouchEvent(event);
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			x_down = (int) event.getX();
+			y_down = (int) event.getY();
+		}
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			int x = (int) event.getX();
+			int y = (int) event.getY();
+			if (Math.abs(x-x_down)==0 && Math.abs(y-y_down)==0) {
+				float mainLeft = header.getLeft()+(header.getWidth()/2)*(1-ViewHelper.getScaleX(header));
+				float mainTop = header.getHeight()/2*(1-ViewHelper.getScaleY(header));
+				float mainButtom = header.getHeight() - mainTop;
+				if (getState().equals(DragState.OPEN) && x>mainLeft && y>mainTop && y<mainButtom ) {
+					close();
+				}
+			}
+		}
 		return true;
 	}
 	
@@ -60,8 +86,40 @@ public class YoutubeLayout3 extends FrameLayout{
 	protected void onLayout(boolean changed, int left, int top, int right,
 			int bottom) {
 		super.onLayout(changed, left, top, right, bottom);
-//		headerLeft = header.getLeft();
 	}
+	
+	@Override
+	public void computeScroll() {
+		if (mDragHelper.continueSettling(true)) {
+			ViewCompat.postInvalidateOnAnimation(this);
+		}
+	}
+	
+	public void close(){
+		if (mDragHelper.smoothSlideViewTo(header, 0, 0)) {
+			ViewCompat.postInvalidateOnAnimation(this);
+		}
+	}
+	
+	public void open() {
+		if (mDragHelper.smoothSlideViewTo(header, maxLeft, 0)) {
+			ViewCompat.postInvalidateOnAnimation(this);
+		}
+	}
+	
+	public DragState getState() {
+		if (header.getLeft()==0) {
+			return DragState.CLOSE;
+		}else if (header.getLeft()==maxLeft) {
+			return DragState.OPEN;
+		}
+		return DragState.DRAG;
+	}
+	
+	public static enum DragState{
+		CLOSE,DRAG,OPEN
+	}
+	
 	
 	private class DragHelperCallback extends ViewDragHelper.Callback{
 
@@ -69,7 +127,8 @@ public class YoutubeLayout3 extends FrameLayout{
 		@Override
 		public boolean tryCaptureView(View arg0, int arg1) {
 			// TODO Auto-generated method stub
-			return arg0.getId()==header.getId();
+//			return arg0.getId()==header.getId();
+			return true;
 		}
 		
 		@Override
@@ -81,27 +140,45 @@ public class YoutubeLayout3 extends FrameLayout{
 //			header.setPivotY(header.getHeight());
 			
 			//设置缩放级别
-			
-            float scaleM = 1 - MAINSCALERATE*left/header.getWidth();
-			header.setScaleX(scaleM);
-            header.setScaleY(scaleM);
+			if (header.getId() == changedView.getId()) {
+				float scaleM = 1 - MAINSCALERATE*left/header.getWidth();
+//			header.setScaleX(scaleM);
+//          header.setScaleY(scaleM);
+				ViewHelper.setScaleX(header, scaleM);
+				ViewHelper.setScaleY(header, scaleM);
 //            desc.setPivotX(0);
 //            desc.setPivotY(desc.getMeasuredHeight()/2f);
-            /**
-             * 计算出主界面缩放后left的真实坐标
-             * 使主界面的left位于屏幕0.7位置时，菜单缩放级别为1
-             */
-			float scaleMenu = (1-MAINLOCATPERSONT/2)
-					+ (left + header.getWidth() * (1 - scaleM) / 2)
-					/ header.getWidth()/2;
-			/**
-			 * 负值为向左偏移，左菜单缩放级别为1时，偏移量为0，偏移量为菜单的宽度比例
-			 */
-			desc.setTranslationX(-(1-scaleMenu)*desc.getWidth());
-			desc.setScaleX(scaleMenu);
-			desc.setScaleY(scaleMenu);
+				/**
+				 * 计算出主界面缩放后left的真实坐标
+				 * 使主界面的left位于屏幕0.7位置时，菜单缩放级别为1
+				 */
+				float scaleMenu = (1-MAINLOCATPERSONT/2)
+						+ (left + header.getWidth() * (1 - scaleM) / 2)
+						/ header.getWidth()/2;
+				/**
+				 * 负值为向左偏移，左菜单缩放级别为1时，偏移量为0，偏移量为菜单的宽度比例
+				 */
+//			desc.setTranslationX(-(1-scaleMenu)*desc.getWidth());
+				ViewHelper.setTranslationX(desc, -(1-scaleMenu)*desc.getWidth());
+//			desc.setScaleX(scaleMenu);
+//			desc.setScaleY(scaleMenu);
+				ViewHelper.setScaleX(desc, scaleMenu);
+				ViewHelper.setScaleY(desc, scaleMenu);
 //            desc.layout(0, 0, desc.getMeasuredWidth(), desc.getMeasuredHeight());
 //            requestLayout();
+			}else {
+//				float scaleMenu = (desc.getWidth()+left*1f)/desc.getWidth();
+				float scaleMenu = left*1f/desc.getWidth()+1;
+				ViewHelper.setScaleX(desc, scaleMenu);
+				ViewHelper.setScaleY(desc, scaleMenu);
+				Log.i("py", left+ "="+scaleMenu);
+				float mainLeft = (scaleMenu - (1-MAINLOCATPERSONT/2))*2*header.getWidth()*header.getWidth()*2/(header.getWidth()*2 + header.getWidth() * MAINSCALERATE);
+				Log.i("py", ""+mainLeft);
+				ViewHelper.setTranslationX(header, mainLeft);
+				float scaleM = 1 - MAINSCALERATE*mainLeft/header.getWidth();
+				ViewHelper.setScaleX(header, scaleM);
+				ViewHelper.setScaleY(header, scaleM);
+			}
 		}
 		
 		@Override
@@ -113,15 +190,38 @@ public class YoutubeLayout3 extends FrameLayout{
 			//经计算得出右边界公式为 2*0.7*header.getWidth()/(2+0.5)
 			//经计算得出右边界公式为 2*MAINLOCATPERSONT*header.getWidth()/(2+MAINSCALERATE)
 //			double maxLeft = 2*0.7*header.getWidth()/(2+0.5);
-			double maxLeft = 0.56*header.getWidth();
-			if (left>maxLeft) {
-				return (int) (0.56*header.getWidth());
-			}
-			if (left<=0) {
-				return 0;
+			
+			if (child.getId() == header.getId()) {
+				maxLeft = (int) (0.56*header.getWidth());
+				if (left>maxLeft) {
+					return (int) (0.56*header.getWidth());
+				}
+				if (left<=0) {
+					return 0;
+				}
+			}else {
+				if (child.getRight()+dx>header.getWidth()) {
+					return 0;
+				}
 			}
 			return left;
 		}
 		
+		
+		@Override
+		public void onViewReleased(View releasedChild, float xvel, float yvel) {
+			super.onViewReleased(releasedChild, xvel, yvel);
+			if (xvel > 0) {
+				open();
+			} else if (xvel < 0) {
+				close();
+			} else if (releasedChild == header && header.getLeft() > header.getWidth() * 0.45) {
+				open();
+			} else if (releasedChild == desc && header.getLeft() > header.getWidth() * 0.55) {
+				open();
+			} else {
+				close();
+			}
+		}
 	}
 }
