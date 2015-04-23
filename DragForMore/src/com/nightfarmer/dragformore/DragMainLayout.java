b7e937e393586e6817f9a069c42d2af5d6a74370
NightFarmer <494090597@qq.com>
2015-04-24 00:51:50 +0800

    at home

--- DragForMore/src/com/nightfarmer/dragformore/DragMainLayout.java
+++ DragForMore/src/com/nightfarmer/dragformore/DragMainLayout.java
@@ -1,26 +1,54 @@
 package com.nightfarmer.dragformore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
 
-public class DragMainLayout extends RelativeLayout implements Dragable{
+public class DragMainLayout extends RelativeLayout implements Dragable {
 	private State state = State.NORMAL;
 	private LoadedListener listener;
+	private ListView listview;
+	private Context context;
 
 	public DragMainLayout(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		// TODO Auto-generated constructor stub
+		this.context = context;
 	}
 
 	public DragMainLayout(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		this(context, attrs, 0);
 		// TODO Auto-generated constructor stub
 	}
 
 	public DragMainLayout(Context context) {
-		super(context);
+		this(context, null);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onFinishInflate() {
+		// TODO Auto-generated method stub
+		listview = (ListView) findViewById(R.id.mylistview);
+		String[] from = { "Text", "Button" };
+		int[] to = { R.id.text, R.id.button };
+		List<Map<String, ?>> list = new ArrayList<Map<String, ?>>();
+		for (int i = 0; i < 20; i++) {
+			Map<String, String> m = new HashMap<String, String>();
+			m.put("Text", "Text" + i);
+			m.put("Button", "Button" + i);
+			list.add(m);
+		}
+		listview.setAdapter(new SimpleAdapter(context, list, R.layout.listitem,
+				from, to));
 	}
 
 	@Override
@@ -28,7 +56,7 @@ public class DragMainLayout extends RelativeLayout implements Dragable{
 		// TODO Auto-generated method stub
 		state = Dragable.State.LOADING;
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
@@ -45,7 +73,6 @@ public class DragMainLayout extends RelativeLayout implements Dragable{
 			}
 		}).start();
 	}
-
 
 	@Override
 	public State getState() {
@@ -67,5 +94,22 @@ public class DragMainLayout extends RelativeLayout implements Dragable{
 		return true;
 	}
 
-
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		// TODO Auto-generated method stub
+//		return super.onInterceptTouchEvent(ev);
+//		return true;
+//		if (listview.getCount() == 0)
+//		{
+//			// 没有item的时候也可以下拉刷新
+//			return true;
+//		} else if (listview.getFirstVisiblePosition() == 0
+//				&& getChildAt(0).getTop() >= 0)
+//		{
+//			// 滑到ListView的顶部了
+//			return true;
+//		} else
+			return super.onInterceptTouchEvent(ev);
+	}
 }
--- DragForMore/src/com/nightfarmer/dragformore/LoadingTopLayout.java
+++ DragForMore/src/com/nightfarmer/dragformore/LoadingTopLayout.java
@@ -1,19 +1,33 @@
 package com.nightfarmer.dragformore;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class LoadingTopLayout extends RelativeLayout{
 
 	private TextView loadingMsg;
-	public State state;
+	private TextView loadedMsg;
+	private View loading_view;
+	public State state = null;
+	private View loaded_result;
+	private Context context;
+	private View loading_icon;
+	private RotateAnimation rotateAnimation_up;
+	private RotateAnimation rotateAnimation_down_0;
+	private RotateAnimation rotateAnimation_down;
+	private View pull_icon;
+	private RotateAnimation refreshingAnimation;
 
 	public LoadingTopLayout(Context context, AttributeSet attrs,
 			int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		this.context = context;
 	}
 
 	public LoadingTopLayout(Context context, AttributeSet attrs) {
@@ -27,21 +41,65 @@ public class LoadingTopLayout extends RelativeLayout{
 	@Override
 	protected void onFinishInflate() {
 		loadingMsg = (TextView) findViewById(R.id.loading_msg);
+		loadedMsg = (TextView) findViewById(R.id.loaded_msg);
+		loading_view = findViewById(R.id.loading_view);
+		loaded_result = findViewById(R.id.load_result);
+		loading_icon = findViewById(R.id.loading_icon);
+		pull_icon = findViewById(R.id.pull_icon);
+		try {
+			rotateAnimation_up = (RotateAnimation) AnimationUtils.loadAnimation(
+					context, R.anim.reverse_anim);
+			rotateAnimation_down = (RotateAnimation) AnimationUtils.loadAnimation(
+					context, R.anim.reverse_anim2);
+			refreshingAnimation = (RotateAnimation) AnimationUtils.loadAnimation(
+					context, R.anim.rotate_sircle);
+			rotateAnimation_down_0 = (RotateAnimation) AnimationUtils.loadAnimation(
+					context, R.anim.reverse_anim0);
+			LinearInterpolator lir = new LinearInterpolator();
+			refreshingAnimation.setInterpolator(lir);
+			rotateAnimation_up.setInterpolator(lir);
+			rotateAnimation_down.setInterpolator(lir);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	
 	/**
 	 * 设置状态：释放刷新
 	 */
 	public void setStateToDown(){
+//		if (State.UP == state) {
+		pull_icon.startAnimation(rotateAnimation_up);
+//		}
+		
 		state = State.DOWN;
-		loadingMsg.setText("释放刷新");
+		
+		loaded_result.setVisibility(View.GONE);
+		loading_view.setVisibility(View.VISIBLE);
+		loading_icon.clearAnimation();
+		loading_icon.setVisibility(View.GONE);
+		pull_icon.setVisibility(View.VISIBLE);
+		
+		loadingMsg.setText("释放立即刷新");
 	}
 	
 	/**
 	 * 设置状态：下拉刷新
 	 */
 	public void setStateToUp(){
+		if (state == null || state == State.LOADED) {
+			pull_icon.startAnimation(rotateAnimation_down_0);
+		}else {
+			pull_icon.startAnimation(rotateAnimation_down);
+		}
 		state = State.UP;
+		
+		loaded_result.setVisibility(View.GONE);
+		loading_view.setVisibility(View.VISIBLE);
+		loading_icon.clearAnimation();
+		loading_icon.setVisibility(View.GONE);
+		pull_icon.setVisibility(View.VISIBLE);
+		
 		loadingMsg.setText("下拉刷新");
 	}
 	
@@ -50,7 +108,11 @@ public class LoadingTopLayout extends RelativeLayout{
 	 */
 	public void setStateToLoading(){
 		state = State.LOADING;
-		loadingMsg.setText("正在刷新");
+		loadingMsg.setText("正在刷新...");
+		loading_icon.startAnimation(refreshingAnimation);
+		loading_icon.setVisibility(View.VISIBLE);
+		pull_icon.clearAnimation();
+		pull_icon.setVisibility(View.GONE);
 	}
 	
 	/**
@@ -58,11 +120,14 @@ public class LoadingTopLayout extends RelativeLayout{
 	 */
 	public void setStateToLoaded(boolean success){
 		state = State.LOADED;
+		
+		loading_view.setVisibility(View.GONE);
+		loaded_result.setVisibility(View.VISIBLE);
 		String msg = "刷新完成";
 		if (!success) {
 			msg = "刷新超时";
 		}
-		loadingMsg.setText(msg);
+		loadedMsg.setText(msg);
 	}
 	
 	public enum State{
