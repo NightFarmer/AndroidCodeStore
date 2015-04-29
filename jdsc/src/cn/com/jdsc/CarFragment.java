36f23868bbeecb86dd75618d877e14106b720ccc
NightFarmer <494090597@qq.com>
2015-04-29 16:03:35 +0800

    新增京东多页签切换框架

---
+++ jdsc/src/cn/com/jdsc/CarFragment.java
@@ -0,0 +1,17 @@
+package cn.com.jdsc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CarFragment extends Fragment{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		System.out.println("CarFragment");
+		return inflater.inflate(R.layout.main_car, container, false);
+	}
+}
